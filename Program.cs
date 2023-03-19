967bd14b8c1205feae1787a4d04196b4201692ac
zzwxh <102860856+zzwxh@users.noreply.github.com>
2023-03-20 00:24:54 +0800

    add v6

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 while(true) {
     ﻿System.Console.WriteLine("enter domain name:");
     var address = System.Console.ReadLine()!;
-    var ipv4 = (await System.Net.Dns.GetHostAddressesAsync(address,System.Net.Sockets.AddressFamily.InterNetwork)).FirstOrDefault();
-    //var ipv6 = (await System.Net.Dns.GetHostAddressesAsync(address,System.Net.Sockets.AddressFamily.InterNetworkV6)).FirstOrDefault();
-    //System.Console.WriteLine($"v4:{ipv4} v6:{ipv6}");
-    System.Console.WriteLine($"v4:{ipv4}");
+    var ipv4address = (await System.Net.Dns.GetHostAddressesAsync(address,System.Net.Sockets.AddressFamily.InterNetwork)).FirstOrDefault();
+    var ipv6address = (await System.Net.Dns.GetHostAddressesAsync(address,System.Net.Sockets.AddressFamily.InterNetworkV6)).FirstOrDefault();
+    System.Console.WriteLine($"ipv4:{ipv4} ipv6:{ipv6}");
+    //System.Console.WriteLine($"v4:{ipv4}");
 }
