8b5102fb578b77b3893170dfb8aebd657924a44f
zzwxh <102860856+zzwxh@users.noreply.github.com>
2023-03-19 15:26:28 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("enter domain name:");
+var address = System.Console.ReadLine()!;
+while(true) {
+    var ipv4 = (await System.Net.Dns.GetHostAddressesAsync(address,System.Net.Sockets.AddressFamily.InterNetwork)).FirstOrDefault();
+    var ipv6 = (await System.Net.Dns.GetHostAddressesAsync(address,System.Net.Sockets.AddressFamily.InterNetworkV6)).FirstOrDefault();
+    System.Console.WriteLine($"v4:{ipv4} v6:{ipv6}");
+}
